fdeff30f65c6b4dd2e36e9de4783273402834281
Zean-Z <jiwanthapa06@gmail.com>
2021-08-09 23:43:41 +0545

    error fixed

--- LearnToEarn/Learn/LearnToEarn/urls.py
+++ LearnToEarn/Learn/LearnToEarn/urls.py
@@ -10,6 +10,7 @@ def index(request):
 urlpatterns = [
     path('test/', index),
     path('home/', views.home, ),
+    path('', views.home, ),
     path('about/', views.about, ),
     path('contact/', views.contactmessages, name='contact'),
     path('courses/', views.courses),
